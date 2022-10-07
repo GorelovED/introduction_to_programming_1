8ac83cbcf2ea2be79ae4f076df496b4e1a340924
acer-pc-evgeniy <e.v.gen.gorelov@yandex.ru>
2022-10-08 01:06:55 +0400

    Создание папки practical_task_1

---
+++ practical_task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Определение входного числа
+Console.Write("Введите число ");
+int numbers = int.Parse(Console.ReadLine());
+
+// Получение квадрата входного числа
+int result = numbers * numbers;
+
+// Вывод чила в консоль
+Console.WriteLine($"Квадрат числа {numbers} = {result}");
+
+// Получение квадрата числа математической функцией
+int result_2 = Convert.ToInt32(Math.Pow(numbers, 2));
+Console.WriteLine($"Квадрат числа {numbers} = {result_2}");
+
