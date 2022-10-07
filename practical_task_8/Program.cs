f157176a2ac9c424b77508b8bfc360b721cb8517
acer-pc-evgeniy <e.v.gen.gorelov@yandex.ru>
2022-10-08 02:22:36 +0400

    Создание папки practical_task_8

---
+++ practical_task_8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите число");
+string input1 = Console.ReadLine();
+int number1 = Convert.ToInt32(input1);
+int count = 2;
+
+Console.Write("Все чётные числа до вашего числа: ");
+
+while (count <= number1)
+{
+Console.Write(count + " ");
+count = count + 2;
+}
